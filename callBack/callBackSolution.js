6a858a3b9755b7f1da64a6c83089ebaf8a7d5b7a
Fatih Gun <fgun@citywire.co.uk>
2023-04-30 17:06:20 +0100

    add: arrow expressions

--- callBack/callBackSolution.js
+++ callBack/callBackSolution.js
@@ -27,3 +27,10 @@ waitingForSomeServerToAct(functionThatNeedsBackEndData);
 /**
  * There are some JS commands like alert, prompt : these will work only in browsers with Live Server
  */
+
+let userName = prompt("Enter username");
+console.log(userName);
+
+let sayHi = () => alert("Hello!");
+
+sayHi();
