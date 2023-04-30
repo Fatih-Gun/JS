8c263a340def066b1ecfb05801274ec17e7ed92c
Fatih Gun <fgun@citywire.co.uk>
2023-04-30 14:51:11 +0100

    add:function details

--- 06_Functions.js
+++ 06_Functions.js
@@ -1,56 +1,53 @@
+// no access modifier, no method overloading
+function add(a,b) {  // parameters of function
+    console.log("Total is : "+(a+b));  // NO return type
+}
+
+add(10,20); // call a function -- argument we are passing
+
+// return type functions
+function multiply(a,b) { 
+    return (a*b);
+}
+
+console.log(multiply(10,20));
+
+// no overloading
+add(10,20,30,200); // ignores after the second parameter
+
 /**
- * There is no Collection in JS. In JS we only have Arrays and Map
- * NOT Fixed size
- * There are two important Brackets in JS 
- * 1. [] ----> creates an Array
- * 2. {} ----> creates an object
+ * 
+ * @param {*} a 
+ * @param {*} b 
+ * @returns 
+ * They call functions as first class citizens of Javascript
+ *  1. you can pass arguments to other functions
+ * 2. you can return a function from a function
+ * 3 . you can store a function into a varible
  */
-
-let scores = new Array(); // creates an empty Array
-let numbers = new Array(10); // creates an array with size 10
-let emptyArray = []; // creates an empty Array
-let myNumbers = new Array(1,2,3,5,8,13);
-let colors = ['red','green','blue'];
-
-
-console.log(scores);
-console.log(numbers);
-console.log(emptyArray);
-console.log(myNumbers);
-console.log(colors);
-
-// print second element in an array
-console.log(colors[1]); // indexed based
-
-// add element to an Array at the end
-colors.push("white");
-console.log(colors);
-
-// add element to beginnig of the Array
-colors.unshift("orange"); 
-console.log(colors);
-
-console.log(colors.pop()); // giving the last element and removing at the same time: white
-console.log(colors); 
-
-// the size of the array
-console.log(colors.length);
-for (let i = 0; i < colors.length ; i++){
-   console.log(colors[i]);
+let divide = function (a,b) {  // you can pass arguments to other functions
+    return a/b;  // you can return a function from a function
 }
 
-// loop with values of Array
-for (let value of colors) {
-    console.log(value);
+console.log(divide(30,5));
+
+let anyFunction = divide;
+
+console.log(anyFunction(30,5));
+
+
+// pass a function as an argument to other functions
+
+function sum(a,b) {
+    return a+b;
 }
 
-colors.forEach(element => {
-    console.log(element);
-});
+function average(a,b,fn) {
+    return fn(a,b)/2;
+}
 
-// can we add multiple type of Data? YES
-let soupOfData = [5, "Oscar", true, undefined]; // you can also put Map, Object, Functions inside Array
-soupOfData.forEach(element => {
-    console.log(typeof element);
-    
-});
+let aveResult = average(10,20,sum);
+console.log(aveResult);
+
+aveResult = average(10,20,multiply);
+console.log(aveResult);
